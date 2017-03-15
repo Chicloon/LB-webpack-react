6bf121bddee0ec8a05db4aa3d83832bee30e71ce
Efim Khlebnyy <Chicloon@gmail.com>
2017-03-15 09:57:44 +0900

    LB fixes

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 const Layout = props =>
   <div id='Layout'>
+    <div className="pure-menu pure-menu-horizontal pure-menu-fixed">
+      <a href="#" className="pure-menu-heading">Invoiced</a>
+      <ul className="pure-menu-list">
+        <li className="pure-menu-item">
+          <a href="#" className="pure-menu-item">Sign In</a>
+        </li>
+      </ul>
+    </div>
     {props.children}
   </div>;
 
